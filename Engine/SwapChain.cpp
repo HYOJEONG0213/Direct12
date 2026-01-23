025f003844d402f04a9d4f688774218facb6f05e
HYOJEONG0213 <fredflem@naver.com>
2026-01-23 20:46:43 +0900

    feat: SwapChian 제작 #1
    1. SwapChain : 그림 외주 맡긴후 Buffer에 저장 후, SwapIndex로 스왑하기

--- Engine/SwapChain.cpp
+++ Engine/SwapChain.cpp
@@ -1,2 +1,48 @@
 #include "pch.h"
 #include "SwapChain.h"
+
+void SwapChain::Init(const WindowInfo& info, ComPtr<IDXGIFactory> dxgi, ComPtr<ID3D12CommandQueue> cmdQueue)
+{
+	// 이전에 만든 정보 날린다
+	_swapChain.Reset();
+
+	//버퍼 어떻게 만들지 묘사하기 
+	DXGI_SWAP_CHAIN_DESC sd;
+	sd.BufferDesc.Width = static_cast<uint32>(info.width); // 버퍼의 해상도 너비
+	sd.BufferDesc.Height = static_cast<uint32>(info.height); // 버퍼의 해상도 높이
+	sd.BufferDesc.RefreshRate.Numerator = 60; // 화면 갱신 비율
+	sd.BufferDesc.RefreshRate.Denominator = 1; // 화면 갱신 비율
+	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM; // 버퍼의 디스플레이 형식
+	sd.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	sd.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+	sd.SampleDesc.Count = 1; // 멀티 샘플링 OFF
+	sd.SampleDesc.Quality = 0;
+	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT; // 후면 버퍼에 렌더링할 것 
+	//버퍼카운터
+	sd.BufferCount = SWAP_CHAIN_BUFFER_COUNT; // 전면+후면 버퍼
+	sd.OutputWindow = info.hwnd;
+	sd.Windowed = info.windowed;
+	// 전면 후면 버퍼 교체 시 이전 프레임 정보 버림
+	sd.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD; 
+	sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+
+
+	//dxgi: device 출력과 관련된 부분들, &_swapChin : 결과물을 여기에 알려줘 
+	dxgi->CreateSwapChain(cmdQueue.Get(), &sd, &_swapChain);
+
+	//버퍼를 _renderTargets 에 넣어준다.. 
+	for (int32 i = 0; i < SWAP_CHAIN_BUFFER_COUNT; i++)
+		_swapChain->GetBuffer(i, IID_PPV_ARGS(&_renderTargets[i]));
+
+}
+
+//현재 화면에 어떤게 표시되어야하는지, 표시하기 
+void SwapChain::Present()
+{
+	_swapChain->Present(0, 0);
+}
+
+void SwapChain::SwapIndex()
+{
+	_backBufferIndex = (_backBufferIndex + 1) % SWAP_CHAIN_BUFFER_COUNT;
+}
