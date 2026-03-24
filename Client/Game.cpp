d6cbcd4174a51ee99041ef6cb29a16296545e966
HYOJEONG0213 <fredflem@naver.com>
2026-03-24 15:36:09 +0900

    feat: Game.cpp 에서 머테리얼 쓰도록 수정 #7
    1. 우선 material 을 set한다음에
    2. mesh->SetMaterial 하자!

--- Client/Game.cpp
+++ Client/Game.cpp
@@ -1,32 +1,32 @@
 #include "pch.h"
 #include "Game.h"
 #include "Engine.h"
+#include "Material.h"
 
-shared_ptr<Mesh>	mesh = make_shared<Mesh>();
-shared_ptr<Shader>	shader = make_shared<Shader>();
-shared_ptr<Texture> texture = make_shared<Texture>();
+shared_ptr<Mesh> mesh = make_shared<Mesh>();
 
 void Game::Init(const WindowInfo &info)
 {
 	GEngine->Init(info);
 
 	vector<Vertex> vec(4);
-	vec[0].pos = Vec3(-0.5f, 0.5f, 0.5f);
-	vec[0].color = Vec4(1.f, 0.f, 0.f, 1.f);
-	vec[0].uv = Vec2(0.f, 0.f);
-	;
+	{
+		vec[0].pos = Vec3(-0.5f, 0.5f, 0.5f);
+		vec[0].color = Vec4(1.f, 0.f, 0.f, 1.f);
+		vec[0].uv = Vec2(0.f, 0.f);
 
-	vec[1].pos = Vec3(0.5f, 0.5f, 0.5f);
-	vec[1].color = Vec4(0.f, 1.f, 0.f, 1.f);
-	vec[1].uv = Vec2(1.f, 0.f);
+		vec[1].pos = Vec3(0.5f, 0.5f, 0.5f);
+		vec[1].color = Vec4(0.f, 1.f, 0.f, 1.f);
+		vec[1].uv = Vec2(1.f, 0.f);
 
-	vec[2].pos = Vec3(0.5f, -0.5f, 0.5f);
-	vec[2].color = Vec4(0.f, 0.f, 1.f, 1.f);
-	vec[2].uv = Vec2(1.f, 1.f);
+		vec[2].pos = Vec3(0.5f, -0.5f, 0.5f);
+		vec[2].color = Vec4(0.f, 0.f, 1.f, 1.f);
+		vec[2].uv = Vec2(1.f, 1.f);
 
-	vec[3].pos = Vec3(-0.5f, -0.5f, 0.5f);
-	vec[3].color = Vec4(0.f, 1.f, 0.f, 1.f);
-	vec[3].uv = Vec2(0.f, 1.f);
+		vec[3].pos = Vec3(-0.5f, -0.5f, 0.5f);
+		vec[3].color = Vec4(0.f, 1.f, 0.f, 1.f);
+		vec[3].uv = Vec2(0.f, 1.f);
+	}
 
 	vector<uint32> indexVec;
 	{
@@ -40,13 +40,20 @@ void Game::Init(const WindowInfo &info)
 		indexVec.push_back(3);
 	}
 
-	// 해당 버퍼 만들어줄래?
 	mesh->Init(vec, indexVec);
 
-	// 만든 셰이더를 읽어줄래?
+	shared_ptr<Shader>	shader = make_shared<Shader>();
+	shared_ptr<Texture> texture = make_shared<Texture>();
 	shader->Init(L"..\\Resources\\Shader\\default.hlsl");
-
 	texture->Init(L"..\\Resources\\Texture\\test.jpg");
+
+	shared_ptr<Material> material = make_shared<Material>();
+	material->SetShader(shader);
+	material->SetFloat(0, 0.3f);
+	material->SetFloat(1, 0.4f);
+	material->SetFloat(2, 0.3f);
+	material->SetTexture(0, texture);
+	mesh->SetMaterial(material);
 
 	// 동기화 안되었을 수 있으니 잠시 대기~
 	GEngine->GetCmdQueue()->WaitSync();
@@ -58,8 +65,6 @@ void Game::Update()
 
 	GEngine->RenderBegin();
 
-	shader->Update();
-
 	{
 		static Transform t = {};
 
@@ -69,8 +74,6 @@ void Game::Update()
 		if (INPUT->GetButton(KEY_TYPE::D)) t.offset.x += 1.f * DELTA_TIME;
 
 		mesh->SetTransform(t);
-
-		mesh->SetTexture(texture);
 
 		mesh->Render();
 	}
@@ -84,8 +87,6 @@ void Game::Update()
 
 		mesh->Render();
 	}*/
-
-	mesh->Render();
 
 	GEngine->RenderEnd();
 }
