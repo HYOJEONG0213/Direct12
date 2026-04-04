3186ef48cf946bcb9e9ea1187cab59b019308fad
HYOJEONG0213 <fredflem@naver.com>
2026-04-04 22:33:28 +0900

    modify: SimpleMath 라이브러리 추가 #10
    1, 카메라 만들거니깐 행렬연산 도와주는 라이브러리를 임포트해준다 !

--- Engine/EnginePch.h
+++ Engine/EnginePch.h
@@ -19,6 +19,7 @@ namespace fs = std::filesystem;
 
 #include "d3dx12.h"
 #include <d3d12.h>
+#include "SimpleMath.h"
 #include <wrl.h>
 #include <d3dcompiler.h>
 #include <dxgi.h>
@@ -53,10 +54,10 @@ using uint8 = unsigned __int8;
 using uint16 = unsigned __int16;
 using uint32 = unsigned __int32;
 using uint64 = unsigned __int64;
-using Vec2 = XMFLOAT2;
-using Vec3 = XMFLOAT3;
-using Vec4 = XMFLOAT4;
-using Matrix = XMMATRIX;
+using Vec2 = DirectX::SimpleMath::Vector2;
+using Vec3 = DirectX::SimpleMath::Vector3;
+using Vec4 = DirectX::SimpleMath::Vector4;
+using Matrix = DirectX::SimpleMath::Matrix;
 
 enum class CBV_REGISTER : uint8
 {
@@ -124,7 +125,7 @@ public:                                                                         
 #define ROOT_SIGNATURE GEngine->GetRootSignature()->GetRootSignature()
 
 #define INPUT GET_SINGLE(Input)
-#define DELTA_TIME GET_SINGLE(Timer);
+#define DELTA_TIME GET_SINGLE(Timer)->GetDeltaTime()
 
 #define CONST_BUFFER(type) GEngine->GetConstantBuffer(type)
 
