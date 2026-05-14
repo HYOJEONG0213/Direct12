6348eb4a93efa2000ecf763d5940c8a30deecbfa
HYOJEONG0213 <fredflem@naver.com>
2026-05-15 02:19:31 +0900

    feat: ShaderInfo에 SHADER_TYPE, TOPOLOGY_TYPE 추가 #17
    1. deferred, forward 렌더링 분리 위해 셰이더 타입 추가
    2. 메시 전달시 사용할 TOPOLOGY_TYPE 속성 추가
    3. 셰이더 타입에 따라 파이프라인 초기화 로직 다르게 수정

--- Engine/Shader.h
+++ Engine/Shader.h
@@ -4,26 +4,34 @@
 // 일감 기술서 : 외주 인력들이 뭘 해야할지 기술함
 // 단계별 기술서
 
-enum class RASTERIZER_TYPE
+enum class SHADER_TYPE : uint8
 {
-	CULL_NONE,	// 모두 연산
-	CULL_FRONT,	// 시계방향 무시
-	CULL_BACK,	// 반시계방향 무시 
-	WIREFRAME,	// 외각선만 보임 
+	DEFERRED,
+	FORWARD,
 };
 
-enum class DEPTH_STENCIL_TYPE	// 깊이테스트
+enum class RASTERIZER_TYPE : uint8
 {
-	LESS,	// 깊이값이 작을때만 그려주기 
-	LESS_EQUAL,	// 같을때도 
+	CULL_NONE,	// 모두 연산
+	CULL_FRONT, // 시계방향 무시
+	CULL_BACK,	// 반시계방향 무시
+	WIREFRAME,	// 외각선만 보임
+};
+
+enum class DEPTH_STENCIL_TYPE : uint8 // 깊이테스트
+{
+	LESS,		// 깊이값이 작을때만 그려주기
+	LESS_EQUAL, // 같을때도
 	GREATER,
 	GREATER_EQUAL,
 };
 
 struct ShaderInfo
 {
-	RASTERIZER_TYPE	   rasterizerType = RASTERIZER_TYPE::CULL_BACK;
-	DEPTH_STENCIL_TYPE depthStencilType = DEPTH_STENCIL_TYPE::LESS;
+	SHADER_TYPE					  shaderType = SHADER_TYPE::FORWARD;
+	RASTERIZER_TYPE				  rasterizerType = RASTERIZER_TYPE::CULL_BACK;
+	DEPTH_STENCIL_TYPE			  depthStencilType = DEPTH_STENCIL_TYPE::LESS;
+	D3D12_PRIMITIVE_TOPOLOGY_TYPE topologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
 };
 
 class Shader : public Object
@@ -35,6 +43,8 @@ public:
 	void Init(const wstring &path, ShaderInfo info = ShaderInfo());
 	void Update();
 
+	SHADER_TYPE GetShaderType() { return _info.shaderType; }
+
 private:
 	void CreateShader(const wstring &path, const string &name, const string &version, ComPtr<ID3DBlob> &blob,
 					  D3D12_SHADER_BYTECODE &shaderByteCode);
@@ -42,6 +52,8 @@ private:
 	void CreatePixelShader(const wstring &path, const string &name, const string &version);
 
 private:
+	ShaderInfo _info;
+
 	ComPtr<ID3DBlob> _vsBlob;
 	ComPtr<ID3DBlob> _psBlob;
 	ComPtr<ID3DBlob> _errBlob;
