92693771cf8576de5cf377395c897e5435740d54
HYOJEONG0213 <fredflem@naver.com>
2026-05-17 22:11:18 +0900

    feat: Shader에 셰이더 타입, 깊이스텐실 타입, 블랜딩 타입 추가 #18
    1. SHADER_TYPE(라이팅), DEPTH_STENCIL_TYPE, BLEND_TYPE 추가
    2. 파이프라인 초기화시 추가된 타입에 맞춰 초기화

--- Engine/Shader.h
+++ Engine/Shader.h
@@ -8,6 +8,7 @@ enum class SHADER_TYPE : uint8
 {
 	DEFERRED,
 	FORWARD,
+	LIGHTING,
 };
 
 enum class RASTERIZER_TYPE : uint8
@@ -24,6 +25,18 @@ enum class DEPTH_STENCIL_TYPE : uint8 // 깊이테스트
 	LESS_EQUAL, // 같을때도
 	GREATER,
 	GREATER_EQUAL,
+	NO_DEPTH_TEST,			// 깊이 테스트(X) + 깊이 기록(O)
+	NO_DEPTH_TEST_NO_WRITE, // 깊이 테스트(X) + 깊이 기록(X)
+	LESS_NO_WRITE,			// 깊이 테스트(O) + 깊이 기록(X)
+};
+
+enum class BLEND_TYPE : uint8
+{
+	// 픽셸셰이더 결과, 렌더 타겟 텍스처 결과 중 어떻게 섞을지
+	DEFAULT,
+	ALPHA_BLEND,
+	ONE_TO_ONE_BLEND,
+	END,
 };
 
 struct ShaderInfo
@@ -31,6 +44,7 @@ struct ShaderInfo
 	SHADER_TYPE					  shaderType = SHADER_TYPE::FORWARD;
 	RASTERIZER_TYPE				  rasterizerType = RASTERIZER_TYPE::CULL_BACK;
 	DEPTH_STENCIL_TYPE			  depthStencilType = DEPTH_STENCIL_TYPE::LESS;
+	BLEND_TYPE					  blendType = BLEND_TYPE::DEFAULT;
 	D3D12_PRIMITIVE_TOPOLOGY_TYPE topologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
 };
 
@@ -40,7 +54,8 @@ public:
 	Shader();
 	virtual ~Shader();
 
-	void Init(const wstring &path, ShaderInfo info = ShaderInfo());
+	void Init(const wstring &path, ShaderInfo info = ShaderInfo(), const string &vs = "VS_Main",
+			  const string &ps = "PS_Main");
 	void Update();
 
 	SHADER_TYPE GetShaderType() { return _info.shaderType; }
