af904b24db47e46773e6ba47be196693deda7436
HYOJEONG0213 <fredflem@naver.com>
2026-06-12 17:06:47 +0900

    feat: Mesh::CreateBonesAndAnimations(), MeshData에 애니 컴포넌트 부착 추가 #28
    1. CreateFromFBX() 에 애니메이션이 있다면 CreateBonesAndAnimation() 함수 실행
    2. CreateBonesAndAnimation() : 뼈대, 프레임별 변환 데이터(SRT)를 GPU 가 사용할 수 있는 버퍼 형태로 변환
    
    3. MeshData::Instantiate(): 애니 컴포넌트 부착 추가

--- Engine/Mesh.h
+++ Engine/Mesh.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Object.h"
+#include "StructuredBuffer.h"
 
 class Material;
 
@@ -13,6 +14,30 @@ struct IndexBufferInfo
 	uint32					count;
 };
 
+struct KeyFrameInfo
+{
+	double time;
+	int32  frame;
+	Vec3   scale;
+	Vec4   rotation;
+	Vec3   translate;
+};
+
+struct BoneInfo
+{
+	wstring boneName;
+	int32	parentIdx;
+	Matrix	matOffset;
+};
+
+struct AnimClipInfo
+{
+	wstring						 animName;
+	int32						 frameCount;
+	double						 duration;
+	vector<vector<KeyFrameInfo>> keyFrames;
+};
+
 class Mesh : public Object
 {
 public:
@@ -23,14 +48,26 @@ public:
 	void Render(uint32 instanceCount = 1, uint32 idx = 0);
 	void Render(shared_ptr<class InstancingBuffer> &buffer, uint32 idx = 0);
 
-	static shared_ptr<Mesh> CreateFromFBX(const struct FbxMeshInfo *meshInfo);
+	static shared_ptr<Mesh> CreateFromFBX(const struct FbxMeshInfo *meshInfo, class FBXLoader &loader);
 
 private:
-	void CreateVertexBuffer(const vector<Vertex> &buffer);
-	void CreateIndexBuffer(const vector<uint32> &buffer);
+	void   CreateVertexBuffer(const vector<Vertex> &buffer);
+	void   CreateIndexBuffer(const vector<uint32> &buffer);
+	void   CreateBonesAndAnimations(class FBXLoader &loader);
+	Matrix GetMatrix(FbxAMatrix &matrix);
 
 public:
-	uint32 GetSubsetCount() { return static_cast<uint32>(_vecIndexInfo.size()); }
+	uint32						GetSubsetCount() { return static_cast<uint32>(_vecIndexInfo.size()); }
+	const vector<BoneInfo>	   *GetBones() { return &_bones; }
+	uint32						GetBoneCount() { return static_cast<uint32>(_bones.size()); }
+	const vector<AnimClipInfo> *GetAnimClip() { return &_animClips; }
+
+	bool						 IsAnimMesh() { return !_animClips.empty(); }
+	shared_ptr<StructuredBuffer> GetBoneFrameDataBuffer(int32 index = 0)
+	{
+		return _frameBuffer[index];
+	} // 전체 본 프레임 정보
+	shared_ptr<StructuredBuffer> GetBoneOffsetBuffer() { return _offsetBuffer; }
 
 private:
 	ComPtr<ID3D12Resource>	 _vertexBuffer;
@@ -38,4 +75,11 @@ private:
 	uint32					 _vertexCount = 0;
 
 	vector<IndexBufferInfo> _vecIndexInfo;
+
+	// Animation
+	vector<AnimClipInfo> _animClips;
+	vector<BoneInfo>	 _bones;
+
+	shared_ptr<StructuredBuffer>		 _offsetBuffer; // 각 뼈의 offset 행렬
+	vector<shared_ptr<StructuredBuffer>> _frameBuffer;	// 전체 본 프레임 정보
 };
