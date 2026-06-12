6aa5f50091bb03307303c1f79a94606f1d87036b
HYOJEONG0213 <fredflem@naver.com>
2026-06-12 17:10:01 +0900

    feat: Animator 클래스 추가 및 GPU에 버퍼 전달 #28
    1. Animator: 프레임 계산 후 StructuredBuffer를 이용해 애니메이션 데이터를 GPU에 전달
    2. Animator::Pushdata() : 컴퓨트 셰이더에 관련 데이터 전달 및 최종 변환 행렬을 u0에 저장
    
    3. StructuredBuffer: 초기 데이터 복사하는 로직 추가(CopyInitialData())

--- Engine/StructuredBuffer.cpp
+++ Engine/StructuredBuffer.cpp
@@ -1,4 +1,4 @@
-﻿#include "pch.h"
+#include "pch.h"
 #include "StructuredBuffer.h"
 #include "Engine.h"
 
@@ -6,7 +6,7 @@ StructuredBuffer::StructuredBuffer() {}
 
 StructuredBuffer::~StructuredBuffer() {}
 
-void StructuredBuffer::Init(uint32 elementSize, uint32 elementCount)
+void StructuredBuffer::Init(uint32 elementSize, uint32 elementCount, void *initialData)
 {
 	_elementSize = elementSize;
 	_elementCount = elementCount;
@@ -21,6 +21,8 @@ void StructuredBuffer::Init(uint32 elementSize, uint32 elementCount)
 
 		DEVICE->CreateCommittedResource(&heapProperties, D3D12_HEAP_FLAG_NONE, &desc, _resourceState, nullptr,
 										IID_PPV_ARGS(&_buffer));
+
+		if (initialData) CopyInitialData(bufferSize, initialData);
 	}
 
 	// SRV
@@ -81,4 +83,41 @@ void StructuredBuffer::PushComputeSRVData(SRV_REGISTER reg)
 void StructuredBuffer::PushComputeUAVData(UAV_REGISTER reg)
 {
 	GEngine->GetComputeDescHeap()->SetUAV(_uavHeapBegin, reg);
+}
+
+void StructuredBuffer::CopyInitialData(uint64 bufferSize, void *initialData)
+{
+	ComPtr<ID3D12Resource> readBuffer = nullptr;
+	D3D12_RESOURCE_DESC	   desc = CD3DX12_RESOURCE_DESC::Buffer(bufferSize, D3D12_RESOURCE_FLAG_NONE);
+	D3D12_HEAP_PROPERTIES  heapProperties = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_UPLOAD);
+
+	DEVICE->CreateCommittedResource(&heapProperties, D3D12_HEAP_FLAG_NONE, &desc, D3D12_RESOURCE_STATE_GENERIC_READ,
+									nullptr, IID_PPV_ARGS(&readBuffer));
+
+	uint8	   *dataBegin = nullptr;
+	D3D12_RANGE readRange{0, 0};
+	readBuffer->Map(0, &readRange, reinterpret_cast<void **>(&dataBegin));
+	memcpy(dataBegin, initialData, bufferSize);
+	readBuffer->Unmap(0, nullptr);
+
+	// Common -> Copy
+	{
+		D3D12_RESOURCE_BARRIER barrier = CD3DX12_RESOURCE_BARRIER::Transition(
+			_buffer.Get(), D3D12_RESOURCE_STATE_COMMON, D3D12_RESOURCE_STATE_COPY_DEST);
+
+		RESOURCE_CMD_LIST->ResourceBarrier(1, &barrier);
+	}
+
+	RESOURCE_CMD_LIST->CopyBufferRegion(_buffer.Get(), 0, readBuffer.Get(), 0, bufferSize);
+
+	// Copy -> Common
+	{
+		D3D12_RESOURCE_BARRIER barrier = CD3DX12_RESOURCE_BARRIER::Transition(
+			_buffer.Get(), D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_COMMON);
+		RESOURCE_CMD_LIST->ResourceBarrier(1, &barrier);
+	}
+
+	GEngine->GetGraphicsCmdQueue()->FlushResourceCommandQueue();
+
+	_resourceState = D3D12_RESOURCE_STATE_COMMON;
 }
