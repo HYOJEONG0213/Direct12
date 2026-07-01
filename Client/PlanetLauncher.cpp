36c96f91d71ca83e87d29707fed0493d870c14e8
HYOJEONG0213 <fredflem@naver.com>
2026-07-01 12:03:27 +0900

    feat: 공발사시 y축은 중력만 영향 받게 수정, 공궤적 ui 추가 #31, #32
    1. 공 발사시 y축을 중력만 영향 받게끔 수정 및 거리조절용 상수 추가
    2. 공궤적 ui 추가(텍스처 및 점 표시 적용)

--- Client/PlanetLauncher.cpp
+++ Client/PlanetLauncher.cpp
@@ -27,15 +27,9 @@ void PlanetLauncher::Update()
 	GET_SINGLE(SceneManager)->GetPickRay(mouse.x, mouse.y, rayOrigin, rayDir);
 
 	float distToPlane = (_spawnZ - rayOrigin.z) / rayDir.z;
-	float worldX = rayOrigin.x + rayDir.x * distToPlane; // 가야하는 z축 양만큼 x축 이동
+	float worldX = rayOrigin.x + rayDir.x * distToPlane; // 마우스 X를 착지 평면상의 월드 X로 변환
 
-	/* 디버깅용, 마우스 위치에 수직으로 행성 떨어트리기!
-	auto planet = PlanetFactory::CreatePlanet(_nextType, Vec3(worldX, _spawnY, _spawnZ));
-	GET_SINGLE(SceneManager)->GetActiveScene()->AddGameObject(planet);
-
-	_nextType = static_cast<PLANET_TYPE>(rand() % (static_cast<int>(PLANET_TYPE::VENUS) + 1));
-
-	UpdatePreview();*/
+	UpdateGuideLine(worldX);
 
 	if (INPUT->GetButtonDown(KEY_TYPE::LBUTTON) && _cooldown <= 0.f)
 	{
@@ -44,14 +38,22 @@ void PlanetLauncher::Update()
 	}
 }
 
+float PlanetLauncher::FallTime() const
+{
+	// s = 1/2 * g * t^2  =>  t = sqrt(2s/g)
+	float gravity = Rigidbody::GetGravity();
+	return sqrtf(2.f * (_launchY - _spawnY) / gravity);
+}
+
 Vec3 PlanetLauncher::ComputeVelocity(float targetX) const
 {
 	Vec3 launchPos = Vec3(0.f, _launchY, _launchZ);
 
 	// X, Z 방향 속도 계산 (등속 직선 운동)
-	float vx = (targetX - launchPos.x) / _t;
-	float vz = (_spawnZ - launchPos.z) / _t;
-	float vy = _launchPower;
+	float t = FallTime();
+	float vx = (targetX - launchPos.x) / t * _powerScale;
+	float vz = (_spawnZ - launchPos.z) / t * _powerScale;
+	float vy = 0.f;
 
 	return Vec3(vx, vy, vz);
 }
@@ -77,6 +79,56 @@ void PlanetLauncher::Throw(float targetX)
 	_currentType = _nextType;
 	_nextType = static_cast<PLANET_TYPE>(rand() % (static_cast<int>(PLANET_TYPE::VENUS) + 1));
 	UpdatePreview();
+}
+
+void PlanetLauncher::UpdateGuideLine(float targetX)
+{
+	if (_current == nullptr) return;
+
+	Vec3  launchPos = _current->GetTransform()->GetLocalPosition();
+	Vec3  velocity = ComputeVelocity(targetX);
+	float gravity = Rigidbody::GetGravity();
+
+	// 처음만 생성!
+	if (_guideDots.empty())
+	{
+		for (int32 i = 0; i < GUIDE_DOT_COUNT; i++)
+		{
+			shared_ptr<GameObject> dot = make_shared<GameObject>();
+			dot->AddComponent(make_shared<Transform>());
+			dot->SetCheckFrustum(false);
+
+			shared_ptr<MeshRenderer> meshRenderer = make_shared<MeshRenderer>();
+			meshRenderer->SetMesh(GET_SINGLE(Resources)->LoadSphereMesh());
+
+			shared_ptr<Texture> texture =
+				GET_SINGLE(Resources)->Load<Texture>(L"GuideLine", L"..\\Resources\\Texture\\GuideLine.png");
+
+			shared_ptr<Material> material = make_shared<Material>();
+			material->SetShader(GET_SINGLE(Resources)->Get<Shader>(L"Texture"));
+			material->SetTexture(0, texture);
+			meshRenderer->SetMaterial(material);
+
+			dot->AddComponent(meshRenderer);
+			GET_SINGLE(SceneManager)->GetActiveScene()->AddGameObject(dot);
+			_guideDots.push_back(dot);
+		}
+	}
+
+	// x = x0 + vx*t, y = y0 + vy*t - 0.5*g*t^2
+	float fallTime = FallTime();
+	for (int32 i = 0; i < GUIDE_DOT_COUNT; i++)
+	{
+		float t = fallTime * (float)i / (float)(GUIDE_DOT_COUNT - 1);
+
+		Vec3 pos;
+		pos.x = launchPos.x + velocity.x * t;
+		pos.y = launchPos.y + velocity.y * t - 0.5f * gravity * t * t;
+		pos.z = launchPos.z + velocity.z * t;
+
+		_guideDots[i]->GetTransform()->SetLocalPosition(pos);
+		_guideDots[i]->GetTransform()->SetLocalScale(Vec3(1.5f, 1.5f, 1.5f));
+	}
 }
 
 void PlanetLauncher::UpdatePreview()
--- Client/PlanetLauncher.h
+++ Client/PlanetLauncher.h
@@ -16,20 +16,25 @@ public:
 	void SetPreviewObject(shared_ptr<class GameObject> obj) { _preview = obj; }
 
 private:
-	void UpdatePreview();
-	void SpawnCurrent();
-	void Throw(float targetX);
-	Vec3 ComputeVelocity(float targetX) const;
+	void  UpdatePreview();
+	void  SpawnCurrent();
+	void  Throw(float targetX);
+	Vec3  ComputeVelocity(float targetX) const;
+	void  UpdateGuideLine(float targetX);
+	float FallTime() const;
+
+	static constexpr int				 GUIDE_DOT_COUNT = 20;
+	vector<shared_ptr<class GameObject>> _guideDots;
 
 	float _cooldown = 0.f;
 
-	float _spawnY = 100.f; // 착지 높이
+	float _spawnY = -30.f; // 착지 높이
 	float _spawnZ = 200.f; // 착지 깊이
 
 	float _launchY = 6.f;	// 발사대 높이
 	float _launchZ = 120.f; // 발사대 깊이
-	float _launchPower = 1.f;
-	float _t = 2.5f;
+
+	float _powerScale = 0.7f; // 거리조절용 상수
 
 	PLANET_TYPE					 _currentType = PLANET_TYPE::MERCURY;
 	PLANET_TYPE					 _nextType = PLANET_TYPE::MERCURY;
