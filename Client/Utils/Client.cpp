c7f3a20ab96e87d881a4a82838ebecebc5209896
HYOJEONG0213 <fredflem@naver.com>
2026-07-28 19:01:15 +0900

    chore: Client 파일 위치 변경
    1. 비주얼 스튜디오처럼 Game, Uitls로 구분

--- Client/Utils/Client.cpp
+++ Client/Utils/Client.cpp
@@ -1,9 +1,9 @@
-﻿// Client.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+// Client.cpp : 애플리케이션에 대한 진입점을 정의합니다.
 //
 #include "pch.h"
 #include "framework.h"
 #include "Client.h"
-#include "Game.h"
+#include "../Game/Game.h"
 #include "UIManager.h"
 #include "imgui_impl_win32.h"
 
